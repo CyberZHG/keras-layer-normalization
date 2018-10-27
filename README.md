f7358df9116a9a4585221a5327fd35a1712e9979
CyberZHG <CyberZHG@gmail.com>
2018-10-27 14:51:56 +0800

    Initialize framework

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# keras-layer-normalization
+# Keras Layer Normalization
+
+[![Travis](https://travis-ci.org/CyberZHG/keras-layer-normalization.svg)](https://travis-ci.org/CyberZHG/keras-layer-normalization)
+[![Coverage](https://coveralls.io/repos/github/CyberZHG/keras-layer-normalization/badge.svg?branch=master)](https://coveralls.io/github/CyberZHG/keras-layer-normalization)
+
+Implementation of the paper: [Layer Normalization](https://arxiv.org/pdf/1607.06450.pdf)
+
+## Install
+
+```bash
+pip install keras-layer-normalization
+```
+
+## Usage
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='keras-layer-normalization',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/CyberZHG/keras-ayer-normalization',
+    license='MIT',
+    author='CyberZHG',
+    author_email='CyberZHG@gmail.com',
+    description='Layer normalization implemented in Keras',
+    long_description=open('README.rst', 'r').read(),
+    install_requires=[
+        'numpy',
+        'keras',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
