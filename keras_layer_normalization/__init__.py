133d1ce3a09574ce2bb2e741bb8e6a54282e8373
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2019-10-29 14:52:10 +0800

    Compatible with tensorflow 2.0

--- keras_layer_normalization/__init__.py
+++ keras_layer_normalization/__init__.py
@@ -1,1 +1,3 @@
 from .layer_normalization import LayerNormalization
+
+__version__ = '0.14.0'
--- keras_layer_normalization/layer_normalization.py
+++ keras_layer_normalization/layer_normalization.py
@@ -67,7 +67,6 @@ class LayerNormalization(keras.layers.Layer):
         return input_mask
 
     def build(self, input_shape):
-        self.input_spec = keras.engine.InputSpec(shape=input_shape)
         shape = input_shape[-1:]
         if self.scale:
             self.gamma = self.add_weight(
